e3a3fe4267f154ec21c60287a1839d55a203d737
Hamza Agar <hamza.agar@d-teknoloji.com.tr>
2025-02-03 00:14:01 +0000

    String Format Extension Added

---
+++ src/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static string Format(this string str, params object[] parameters)
+    {
+        return string.Format(str, parameters);
+    }
+}
